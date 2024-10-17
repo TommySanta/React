5211587f0ce58c8b6001fce72378d2f9fc91704a
TommySanta <tomas.santamaria.benito@gmail.com>
2024-10-17 09:46:52 +0200

    Añadir carpeta primerreact con todos los archivos

---
+++ primerreact/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import Saludo from './components/Saludo';
+
+function App() {
+  var estilo ={
+    color: "blue",
+    backgroundcolor:"yellow"
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 style={estilo}>Ejemplo de estilo</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+  
+}
+
+export default App;
